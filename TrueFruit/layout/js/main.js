beb70ff561f06636229700bb07e215c9f4dd36ba
SKAZOOCHNIK <80123721+SKAZOOCHNIK@users.noreply.github.com>
2022-02-09 23:09:53 +0300

    Add files via upload

--- TrueFruit/layout/js/main.js
+++ TrueFruit/layout/js/main.js
@@ -1,5 +1,5 @@
 const openPopUp = document.getElementById('1');
-const closePopUp =document.getElementById('pop_up_close');
+const closePopUp = document.getElementById('pop_up_close');
 const popUp = document.getElementById('pop_up');
 
 openPopUp.addEventListener('click', function(e){
@@ -10,3 +10,15 @@ openPopUp.addEventListener('click', function(e){
 closePopUp.addEventListener('click', ()=>{
     popUp.classList.remove('active');
 })
+window.onscroll = function showHeader() 
+{
+    let header = document.querySelector(".header");
+    if(window.pageYOffset > 200)
+    {
+        header.classList.add('header_fixed');
+    }
+    else
+    {
+        header.classList.remove('header_fixed');
+    }
+}
